ecd5c4907061d23f772258f3123602324f30565a
JH7770 <wlghks7429@naver.com>
2022-10-24 14:50:08 +0900

    #1 프론트 프로젝트 init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,11 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    'prettier',
-  ],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  env: {
+    node: true,
+    browser: true,
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier'],
 };
