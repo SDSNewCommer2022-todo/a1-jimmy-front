dbcd31706290c37c3b1eac2a8d0bf8bd2b9f6b5f
JH7770 <wlghks7429@naver.com>
2022-10-24 14:15:20 +0900

    #2 eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true, // 세미 콜론 사용 여부
+  trailingComma: 'all', // 여러줄 사용시 후행 콤마 사용 방식
+  singleQuote: true, // single 쿼테이션 사용 여부
+  printWidth: 100, // 줄바꿈 폭 길이
+  tabWidth: 2, // 탭 너비
+  endOfLine: 'auto', // EoF 방식
+  useTabs: false, // 탭 사용 여부
+};
